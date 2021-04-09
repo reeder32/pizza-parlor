aed2de979d0b6229cc07a82090e050e3d807388f
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-09 14:24:54 -0700

    add order summary with bootstrap card

--- js/scripts.js
+++ js/scripts.js
@@ -36,12 +36,36 @@ Pizza.prototype.getPriceForToppings = function () {
 
 $(document).ready(function () {
   let pizza = new Pizza();
+  function showSummary() {
+    console.log("Here");
+    let summaryText = "Your pizza has: cheese, "
+    pizza.toppings.forEach(function (topping, index) {
+      console.log(topping);
+      if (index === pizza.toppings.length - 1) {
+        summaryText = summaryText + topping;
+      } else {
+        summaryText = summaryText + topping + ", ";
+      }
+    });
+    console.log(summaryText);
+    $(".card-title").text(`You ordered a ${pizza.size} pizza!`);
+    $(".card-text").text(summaryText);
+    $("#charge-button").text(`Pay $${pizza.getPriceForSize() + pizza.getPriceForToppings()}`);
+    $("#order-form").slideUp();
+    $("#order-summary").fadeIn();
+  }
   $("#order-form").submit(function (event) {
     let size = $("input[name='sizeRadios']:checked").val();
     pizza.addSize(size);
     let toppings = $("input[name='toppingRadios']:checked").map(function (index, element) {
       pizza.addTopping(element.value);
     });
+    showSummary();
+    event.preventDefault();
+  })
+
+  $("#start-over").click(function (event) {
+    location.reload();
     event.preventDefault();
   })
 });
